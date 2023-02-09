d973363bf0718714c867a46e2a31e5043d2d09ec
IlyasBertram <39627564+IlyasBertram@users.noreply.github.com>
2023-02-09 11:41:13 +0700

    -- feat: adding home page and fixing search option

---
+++ YuGiOh-Play-client/src/components/navbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+              <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+            </a>
+    
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+              <li><a href="#" class="nav-link px-2 text-secondary fs-4">Home</a></li>
+              <li><a href="#" class="nav-link px-2 text-white fs-4">About</a></li>
+            </ul>
+    
+            
+            <!-- kalo belom login muncul ini -->
+            <div class="text-end">
+
+              <!-- <button type="button" class="btn btn-outline-light me-2">Login</button>
+              <button type="button" class="btn btn-warning">Sign-up</button> -->
+
+              <!-- <button type="button" class="btn btn-outline-light me-2">logout</button> -->
+
+            </div>
+
+
+            <!-- kalo sudah login muncul ini -->
+            <!-- <a href="#" class="nav-link text-white">
+              <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"></use></svg>
+              Customers
+            </a> -->
+    </div>
+</template>
--- YuGiOh-Play-client/src/main.js
+++ YuGiOh-Play-client/src/main.js
@@ -1,14 +1,19 @@
-import { createApp } from 'vue'
+import { createApp, markRaw } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import VueAwesomePaginate from "vue-awesome-paginate";
 
 const app = createApp(App)
+const pinia = createPinia();
 
-app.use(createPinia())
+pinia.use(({ store }) => {
+    store.router = markRaw(router);
+})
+
+app.use(pinia)
 app.use(router)
-
+app.use(VueAwesomePaginate)
 app.mount('#app')
